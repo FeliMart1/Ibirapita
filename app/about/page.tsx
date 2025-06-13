fceffc06d303e8a5afbbb7cc65f23ce57e1d8c3f
FeliMart1 <165679914+FeliMart1@users.noreply.github.com>
2025-06-13 12:39:29 -0300

    Delete app directory

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-    return (
-      <main className="p-6 text-center">
-        <h1 className="text-3xl font-bold">Sobre Nosotros</h1>
-        <p className="text-lg text-gray-600 mt-2">Conoce más sobre nuestra empresa.</p>
-      </main>
-    );
-  }
-  
